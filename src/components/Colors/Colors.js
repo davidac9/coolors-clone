5b0a70834521d7ac2172cc0d06fa306015a38255
David Collett <davida.collett95@gmail.com>
2019-09-30 22:53:36 -0600

    changed the colors array and some functions so the length of the colors array could be dynamic also gave each color a lock property

--- src/components/Colors/Colors.js
+++ src/components/Colors/Colors.js
@@ -4,7 +4,8 @@ import './Colors.css'
 export default class Colors extends Component {
     state = {
         hexDigits: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'],
-        colors: []
+        colors: [],
+        amount: 5,
     }
     randomColor = () => {
         return (`#${[this.state.hexDigits[Math.floor(Math.random() * 15)],
@@ -15,14 +16,20 @@ export default class Colors extends Component {
         this.state.hexDigits[Math.floor(Math.random() * 15)]].join('')}`)
     }
     randomColors = () => {
+        let newColors = []
+        for (let i = 0; i < this.state.amount; i++) {
+            newColors[i] = {color:this.randomColor(), lock: false}
+        }
         this.setState({
-            colors: [
-                this.randomColor(),
-                this.randomColor(),
-                this.randomColor(),
-                this.randomColor(),
-                this.randomColor()
-            ]
+            colors: newColors
+        })
+
+    }
+    colorLock = (colorIndex) => {
+        let colorArr = this.state.colors
+        colorArr[colorIndex].lock = !colorArr[colorIndex].lock
+        this.setState({
+            colors: colorArr
         })
     }
     spacePressed = (event) => {
@@ -31,27 +38,20 @@ export default class Colors extends Component {
             this.randomColors()
         }
     }
+
     componentDidMount() {
         this.randomColors()
     }
     render() {
+        const colorMap = this.state.colors.map((el, i) => (
+            <div className={`color${i + 1}`} style={{ backgroundColor: `${this.state.colors[i].color}`}}>
+                {this.state.colors[i].color.toUpperCase()}
+                <button onClick={() => this.colorLock(i)}>lock</button>
+            </div>
+        ))
         return (
             <div className="Colors" onKeyDown={e => this.spacePressed(e)} tabIndex="0">
-                <div className="color-1"
-                    style={{ backgroundColor: `${this.state.colors[0]}` }}
-                >color1</div>
-                <div className="color-2"
-                    style={{ backgroundColor: `${this.state.colors[1]}` }}
-                >color2</div>
-                <div className="color-3"
-                    style={{ backgroundColor: `${this.state.colors[2]}` }}
-                >color3</div>
-                <div className="color-4"
-                    style={{ backgroundColor: `${this.state.colors[3]}` }}
-                >color4</div>
-                <div className="color-5"
-                    style={{ backgroundColor: `${this.state.colors[4]}` }}
-                >color5</div>
+                {colorMap}
             </div>
         )
     }
